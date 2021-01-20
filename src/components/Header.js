ac4625bba57de323d8394f4b8b4ff511d1c47479
Chu-Cheng Fu <ccfu@kth.se>
2021-01-20 16:09:45 +0100

    v0.0.1

---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import { makeStyles } from '@material-ui/core/styles'
+
+
+const Header = () => {
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      color: theme.palette.text.secondary,
+    },
+  }))
+  const classes = useStyles()
+
+  return(
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>Hi, this is charles</Paper>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab label="About" />
+            <Tab label="Projects" />
+            <Tab label="Contact" />
+          </Tabs>
+
+        </Grid>
+      </Grid>
+    </div>
+
+  )
+}
+
+export default Header
